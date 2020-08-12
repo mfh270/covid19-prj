0ff530e2409a13246f394f52245e9663cef30d5e
sarasheed <sarasheed54@gmail.com>
2020-08-12 22:58:01 +0530

    Minor changes

--- covid19/src/app/app.module.ts
+++ covid19/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Modules
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +14,11 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
+// Components
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SymptomsComponent } from './symptoms/symptoms.component';
 import { PrecautionsComponent } from './precautions/precautions.component';
@@ -24,7 +30,6 @@ import { RemediesComponent } from './remedies/remedies.component';
 import { SelfTestComponent } from './self-test/self-test.component';
 import { WorldTrackerComponent } from './world-tracker/world-tracker.component';
 import { IndiaTrackerComponent } from './india-tracker/india-tracker.component';
-import { MatTableModule } from '@angular/material/table';
 import { QaComponent } from './qa/qa.component';
 import { SuggestionsComponent } from './suggestions/suggestions.component';
 
@@ -59,6 +64,8 @@ import { SuggestionsComponent } from './suggestions/suggestions.component';
     MatTooltipModule,
     MatCardModule,
     MatTableModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
